d676228ac39b8d6b50184cc008bed505c2258c4e
Allen Zhou <allenylzhou@gmail.com>
2017-02-21 17:04:10 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import Spinner from 'react-spin';
+import Dropzone from 'react-dropzone';
+import Masonry from 'react-masonry-component';
+import './App.css';
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
+
+function parseJSON(response) {
+  return response.json()
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isUploading: false,
+      displayStr: '',
+      preview: '',
+      faces: []
+    }
+  }
+
+  render() {
+    const { isUploading, preview, displayStr, faces } = this.state
+    return (
+      <div className="App">
+        <Dropzone onDrop={this.onDrop.bind(this)} className={'my-dropzone'}>
+          {
+            isUploading
+            ? <Spinner />
+            : preview.length > 0
+              ? <div>
+                <img src={preview} width='160'/>
+                <p>{displayStr}</p>
+              </div>
+              : <div>Click/tap here to take/upload a picture of your face to see which celebrities you look like</div>
+          }
+        </Dropzone>
+        <Masonry
+          className={'my-gallery-class'} // default ''
+          options={{}} // default {}
+          disableImagesLoaded={false} // default false
+          updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+        >
+          {
+            faces.map((face, i) => {
+              return <div key={i} className="grid-item">
+                <img alt={face.name} src={face.url} />
+                <p>{face.name}</p>
+              </div>
+            })
+          }
+        </Masonry>
+      </div>
+    );
+  }
+
+  onDrop(acceptedFiles, rejectedFiles) {
+    const firstFile = acceptedFiles[0]
+    const body = new FormData()
+    body.append('image', firstFile)
+    this.setState({
+      isUploading: true
+    })
+    fetch('http://cors-anywhere.herokuapp.com/http://faces.thehive.ai/classify', {
+      method: 'POST',
+      body
+    })
+    .then(checkStatus)
+    .then(parseJSON)
+    .then(data => {
+      const faces = data.full_probabilities[0].bounding_boxes.length > 0
+      ? data.full_probabilities[0].bounding_boxes[0].celeb_score_list
+      : []
+      this.setState({
+        isUploading: false,
+        preview: firstFile.preview,
+        displayStr: data.display_string,
+        faces
+      })
+    })
+  }
+}
+
+export default App;
